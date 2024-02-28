0c4c1ad8ba9d7cc9c01bc5f79a684c633dcc4b29
Uday Bari <115573575+UdayBari3421@users.noreply.github.com>
2024-02-28 16:29:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
+let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 function makeid(l) {
-  // write your code here
-}
+	let len = characters.length;
+	let res = ' ';
+	for ( let i = 0; i < len; i++ ) {
+        res += characters.charAt(Math.floor(Math.random() * len));
+    }
+
+	let newres = res.slice(0, l)
+	console.log(newres);
+	return newres;
+} 
 
 // Do not change the code below.
+
 const l = prompt("Enter a number.");
-alert(makeid(l));
+alert(makeid(l));  
